722100bfa2a94ed58126aaf15bc23661457005f6
Bill Robinson <dseevr@users.noreply.github.com>
2016-11-03 11:52:07 -0700

    More WIP work
    copying headers from the upstream response to our response, adding custom response headers, etc.
    
    Signed-off-by: Bill Robinson <dseevr@users.noreply.github.com>

--- rbac_proxy/main.go
+++ rbac_proxy/main.go
@@ -9,29 +9,36 @@ import (
 	"net/url"
 )
 
-// TODO: use a version string which will be sent in response headers and can be
-//       displayed in --version.  it can be set by:
-//       -ldflags '-X main.version=${BUILD_VERSION-devbuild}'
-// var version string
+const (
+	name = "rbac_proxy"
+)
 
-func handler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+var (
+	// flags
+	disableTLS bool
+
+	// globals
+	upstream *url.URL
+
+	// TODO: use a version string which will be sent in response headers and can be
+	//       displayed in --version.  it can be set by:
+	//       -ldflags '-X main.version=${BUILD_VERSION-devbuild}'
+	version = "devbuild"
+)
+
+func proxyRequest(w http.ResponseWriter, req *http.Request) {
 
 	copy := new(http.Request)
 	*copy = *req
-
-	upstream := &url.URL{
-		Scheme: "http",
-		Host:   "localhost",
-	}
 
 	copy.URL = upstream
 
 	// the RequestURI has to be cleared before sending a new request
 	copy.RequestURI = ""
 
-	// TODO: add X-Forwarded-For header
-	// TODO: add X-Forwarded-By (rbac_proxy version)
+	// add our custom headers
+	req.Header.Add("X-Forwarded-For", "w.x.y.z") // TODO: get real ip
+	req.Header.Add("X-Forwarder", name+" "+version)
 
 	resp, err := http.DefaultClient.Do(copy)
 	if err != nil {
@@ -49,14 +56,57 @@ func handler(w http.ResponseWriter, req *http.Request) {
 		panic("request failed")
 	}
 
+	// copy all the response headers into our response
+	for k, headers := range resp.Header {
+		for _, header := range headers {
+			w.Header().Set(k, header)
+		}
+	}
+
 	w.Write(data)
 }
 
-var disableTLS bool
+func handler(w http.ResponseWriter, req *http.Request) {
+	/*
+		authn, err := doLDAPLookup()
+		if err != nil {
+			fmt.Println("LDAP lookup failed:", err)
+			// TODO: log an error and return a 500
+			panic("LDAP lookup failed")
+		}
+
+		if !authn {
+			fmt.Println("LDAP authentication failed")
+			// TODO: log it and return a 401
+			panic("LDAP authentication failed")
+		}
+
+		authz, err := doAuthorization()
+		if err != nil {
+			fmt.Println("authorization failed:", err)
+			// TODO: log an error and return a 500
+			panic("authorization failed")
+		}
+
+		if !authz {
+			fmt.Println("user is not authorized")
+			// TODO: log it and return a 401
+			panic("user is not authorized")
+		}
+	*/
+	proxyRequest(w, req)
+}
 
 func main() {
+	// TODO: add a flag for LDAP host + port
 	flag.BoolVar(&disableTLS, "disable-tls", false, "if set, TLS is disabled")
 	flag.Parse()
+
+	// TODO: set this from envvars or command line flags
+	upstream = &url.URL{
+		Scheme: "http",
+		Host:   "localhost",
+	}
 
 	http.HandleFunc("/", handler)
 
@@ -65,6 +115,7 @@ func main() {
 	if disableTLS {
 		err = http.ListenAndServe(":8000", nil)
 	} else {
+		// TODO: get the cert/key from somewhere else (envvar, flag, Vault, etc.)
 		err = http.ListenAndServeTLS(":8000", "cert.pem", "key.pem", nil)
 	}
 
