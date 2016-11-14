0d1cc4543c4afcaa6152441ad4e87a3360932e44
Selvi Kadirvel <selvikadirvel@gmail.com>
2016-11-14 13:38:12 -0800

    Enabling a data store for AuthN and AuthZ via etcd and consul
    Unit testing:
    - Run etcd and consul locally
    - cd ccn_proxy/state; go test
    
    Signed-off-by: selvikadirvel@gmail.com

---
+++ state/consulstatedriver.go
@@ -0,0 +1,438 @@
+package state
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/hashicorp/consul/api"
+
+	ccnerrors "github.com/contiv/ccn_proxy/common/errors"
+	"github.com/contiv/ccn_proxy/common/types"
+)
+
+// Max times to retry in case of failure
+const maxConsulRetries = 10
+
+// ConsulStateDriver implements the StateDriver interface for a
+// consul-based distributed key-value store used to store any
+// state information needed by ccn proxy
+type ConsulStateDriver struct {
+
+	// client used to access consul
+	Client *api.Client
+}
+
+//
+// Init initializes the state driver with needed configuration
+//
+// Parameters:
+//   config: configuration parameters to create consul client
+//
+// Return values:
+//   error:  error when creating a consul client
+//
+func (d *ConsulStateDriver) Init(config *types.KVStoreConfig) error {
+	var err error
+
+	if config == nil || !strings.Contains(config.StoreURL, "consul://") {
+		return errors.New("Invalid consul config")
+	}
+
+	cfg := api.Config{
+		Address: strings.TrimPrefix(config.StoreURL, "consul://"),
+	}
+
+	// create a consul client
+	d.Client, err = api.NewClient(&cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Deinit is currently a no-op.
+func (d *ConsulStateDriver) Deinit() {
+}
+
+//
+// processKey truncates the leading slash in a key
+// (needed because consul doesn't accept keys with a leading '/')
+//
+// Parameters:
+//   inKey: key string that may have a leading '/'
+//
+// Return value:
+//   string: key string with trimmed leading '/'
+//
+func processKey(inKey string) string {
+	return strings.TrimPrefix(inKey, "/")
+}
+
+//
+// Write a key-value pair to the consul KV store
+//
+// Parameters:
+//   key:    key to be stored
+//   value:  value to be stored
+//
+// Return values:
+//   error: Error when writing a KV pair via the consul client
+//          nil if successful
+//
+func (d *ConsulStateDriver) Write(key string, value []byte) error {
+	key = processKey(key)
+	_, err := d.Client.KV().Put(&api.KVPair{Key: key, Value: value}, nil)
+	if err != nil && (api.IsServerError(err) || strings.Contains(err.Error(), "EOF") ||
+		strings.Contains(err.Error(), "connection refused")) {
+		for i := 0; i < maxConsulRetries; i++ {
+			_, err = d.Client.KV().Put(&api.KVPair{Key: key, Value: value}, nil)
+			if err == nil {
+				break
+			}
+
+			// Retry after a delay
+			time.Sleep(time.Second)
+		}
+	}
+
+	return err
+}
+
+//
+// Read returns the value for a key
+//
+// Parameters:
+//   key:    key for which value is to be retrieved
+//
+// Return values:
+//   []byte: value associated with the given key
+//   error: Error when reading from consul
+//          nil if successful
+//
+func (d *ConsulStateDriver) Read(key string) ([]byte, error) {
+	key = processKey(key)
+	kv, _, err := d.Client.KV().Get(key, nil)
+	if err != nil {
+		if api.IsServerError(err) || strings.Contains(err.Error(), "EOF") ||
+			strings.Contains(err.Error(), "connection refused") {
+			for i := 0; i < maxConsulRetries; i++ {
+				kv, _, err = d.Client.KV().Get(key, nil)
+				if err == nil {
+					break
+				}
+
+				// Retry after a delay
+				time.Sleep(time.Second)
+			}
+		} else {
+			return []byte{}, err
+		}
+	}
+
+	// Consul returns success and a nil kv when a key is not found,
+	// translate it to 'Key not found' error
+	if kv == nil {
+		return []byte{}, ccnerrors.ErrKeyNotFound
+	}
+
+	return kv.Value, err
+}
+
+//
+// ReadAll returns all state for a key
+//
+// Parameters:
+//   key:    key for which all values are to be retrieved
+//
+// Return values:
+//   [][]byte: list of values associated with the given key
+//   error: Error when writing to the KeysAPI of consul client
+//          nil if successful
+//
+func (d *ConsulStateDriver) ReadAll(baseKey string) ([][]byte, error) {
+	baseKey = processKey(baseKey)
+	kvs, _, err := d.Client.KV().List(baseKey, nil)
+	if err != nil {
+		return nil, err
+	}
+	// Consul returns success and a nil kv when a key is not found,
+	// translate it to 'Key not found' error
+	if kvs == nil {
+		return nil, ccnerrors.ErrKeyNotFound
+	}
+
+	values := [][]byte{}
+	for _, kv := range kvs {
+		values = append(values, kv.Value)
+	}
+
+	return values, nil
+}
+
+//
+// channelConsulEvents
+//
+// Parameters:
+//   baseKey:        Key for which any state changes are to be notified
+//   kvCache:        map of already seen keys
+//   chKVPairs:      channel on which change notifications are received
+//   chValueChanges: channel using which any value changes are communicated
+//                   to the caller
+//   chErr:          channel using which any errors are communicated to the
+//                   caller
+//   chStop:         channel using which waiting for events can be stopped
+//
+func (d *ConsulStateDriver) channelConsulEvents(baseKey string, kvCache map[string]*api.KVPair,
+	chKVPairs chan api.KVPairs, chValueChanges chan [2][]byte, chErr chan error, chStop chan bool) {
+	for {
+		select {
+		// block on change notifications
+		case kvs := <-chKVPairs:
+			kvsRcvd := map[string]*api.KVPair{}
+			// Generate Create/Modifiy events for the keys recvd
+			for _, kv := range kvs {
+				// XXX: The logic below assumes that the node returned is always a node
+				// of interest. Eg: If we set a watch on /a/b/c, then we are mostly
+				// interested in changes in that directory i.e. changes to /a/b/c/d1..d2
+				kvsRcvd[kv.Key] = kv
+				valueChange := [2][]byte{nil, nil}
+				valueChange[0] = kv.Value
+				if kvSeen, ok := kvCache[kv.Key]; !ok {
+					log.Debugf("Received create for key: %q, kv: %+v", kv.Key, kv)
+				} else if kvSeen.ModifyIndex != kv.ModifyIndex {
+					log.Debugf("Received modify for key: %q, kv: %+v", kv.Key, kv)
+					valueChange[1] = kvSeen.Value
+				} else {
+					// no changes to the key, skipping
+					log.Debugf("Skipping key with no changes: %s", kv.Key)
+					continue
+				}
+				//update the map of seen keys
+				kvCache[kv.Key] = kv
+
+				//channel the translated response
+				chValueChanges <- valueChange
+			}
+
+			// Generate Delete events for missing keys
+			for key, kv := range kvCache {
+				if _, ok := kvsRcvd[key]; !ok {
+					log.Infof("Received delete for key: %q, Pair: %+v", kv.Key, kv)
+					chValueChanges <- [2][]byte{nil, kv.Value}
+					// remove this key from the map of seen keys
+					delete(kvCache, key)
+				}
+			}
+
+		case <-chStop:
+			log.Infof("Stop request received")
+			return
+		}
+	}
+}
+
+//
+// WatchAll watches value changes for a key in consul
+//
+// Parameters:
+//   baseKey:         key for which changes are to be watched
+//   chValueChanges:  channel that will be used to communicate
+//                    any changes to values of a key
+//
+// Return values:
+//   error: Any error when watching for a value change for a key,
+//          nil if successful
+//
+func (d *ConsulStateDriver) WatchAll(baseKey string, chValueChanges chan [2][]byte) error {
+
+	// trim leading '/' of a key
+	baseKey = processKey(baseKey)
+
+	chKVPairs := make(chan api.KVPairs, 1)
+
+	// channel that will be used to stop watching for state-change events
+	chStop := make(chan bool, 1)
+
+	// channel that will be used to receive errors
+	chErr := make(chan error, 2)
+
+	// Consul returns all the keys as return value of List(). The following map helps
+	// track of state that has been seen and used to appropriately generate
+	// create, modify and delete events
+	kvCache := map[string]*api.KVPair{}
+
+	// read with index=0 to fetch all existing keys
+	var waitIndex uint64
+	kvs, qm, err := d.Client.KV().List(baseKey, &api.QueryOptions{WaitIndex: waitIndex})
+	if err != nil {
+		log.Errorf("consul read failed for key %q. Error: %s", baseKey, err)
+		return err
+	}
+
+	// Consul returns success and a nil kv when a key is not found.
+	// Treat this as starting with no state.
+	// XXX: shall we fail the watch in this case?
+	if kvs == nil {
+		kvs = api.KVPairs{}
+	}
+	for _, kv := range kvs {
+		kvCache[kv.Key] = kv
+	}
+	waitIndex = qm.LastIndex
+
+	go d.channelConsulEvents(baseKey, kvCache, chKVPairs, chValueChanges, chErr, chStop)
+
+	for {
+		select {
+		case err := <-chErr:
+			return err
+		default:
+			kvs, qm, err := d.Client.KV().List(baseKey, &api.QueryOptions{WaitIndex: waitIndex})
+			if err != nil {
+				if api.IsServerError(err) || strings.Contains(err.Error(), "EOF") || strings.Contains(err.Error(), "connection refused") {
+					log.Warnf("Consul watch: server error: %v for %s. Retrying..", err, baseKey)
+					time.Sleep(5 * time.Second)
+					continue
+				} else {
+					log.Errorf("consul watch failed for key %q. Error: %s. stopping watch..", baseKey, err)
+					chStop <- true
+					return err
+				}
+			}
+			// Consul returns success and a nil kv when a key is not found.
+			// This shall translate into appropriate 'Delete' events or
+			// no events (depending on whether some keys were seen before)
+			// XXX: shall we stop the watch in this case?
+			if kvs == nil {
+				kvs = api.KVPairs{}
+			}
+
+			waitIndex = qm.LastIndex
+			chKVPairs <- kvs
+		}
+	}
+}
+
+//
+// ClearState clears the state for a key in consul
+//
+// Parameters:
+//   key: key for which state is to be cleared
+//
+// Return value:
+//   error: Error returned by consul client when deleting a key
+//
+func (d *ConsulStateDriver) ClearState(key string) error {
+	key = processKey(key)
+	_, err := d.Client.KV().Delete(key, nil)
+	return err
+}
+
+//
+// ReadState reads key into a types.State using the provided
+// unmarshaling function
+//
+// Parameters:
+//   key:        key whose value is to be retrieved
+//   value:      retrieved value for the key
+//   unmarshal: function to be used for unmarshaling retrieved
+//               value from a byte slice in to type types.State
+//
+// Return value:
+//   error: Error returned by consul client when reading key's value
+//          or error in unmarshaling key's value
+//
+func (d *ConsulStateDriver) ReadState(key string, value types.State,
+	unmarshal func([]byte, interface{}) error) error {
+	key = processKey(key)
+	encodedState, err := d.Read(key)
+	if err != nil {
+		return err
+	}
+
+	return unmarshal(encodedState, value)
+}
+
+//
+// ReadAllState reads all the state for a baseKey and returns
+// a list of types.State
+//
+// Parameters:
+//   baseKey:    key whose values are to be read
+//   sType:      types.State
+//   unmarshal:  function that is used to convert key's values
+//               from a byte slice to values of type types.State
+//
+// Return values:
+//   []types.State: Retrieved values for a key as type types.State
+//   error:         Any error returned by readAllStateCommon
+//
+func (d *ConsulStateDriver) ReadAllState(baseKey string, sType types.State,
+	unmarshal func([]byte, interface{}) error) ([]types.State, error) {
+	baseKey = processKey(baseKey)
+	return readAllStateCommon(d, baseKey, sType, unmarshal)
+}
+
+//
+// WatchAllState watches all state changes for a key
+//
+// Parameters:
+//    baseKey:   key to be watched
+//    sType:     types.State to convert values to/from
+//    unmarshal: function used to convert values to types.State
+//    chStateChanges:      channel of types.WatchState
+//
+// Return values:
+//    error: Any error when watching all state
+//
+func (d *ConsulStateDriver) WatchAllState(baseKey string, sType types.State,
+	unmarshal func([]byte, interface{}) error, chStateChanges chan types.WatchState) error {
+
+	// trim leading '/' in key
+	baseKey = processKey(baseKey)
+
+	// channel that will be used to communicate value changes
+	chValueChanges := make(chan [2][]byte, 1)
+
+	// channel used to communicate errors
+	chErr := make(chan error, 1)
+
+	go channelStateEvents(d, sType, unmarshal, chValueChanges, chStateChanges, chErr)
+
+	err := d.WatchAll(baseKey, chValueChanges)
+	if err != nil {
+		return err
+	}
+
+	err = <-chErr
+	return err
+
+}
+
+//
+// WriteState writes state for a key into the consul KV store
+//
+// Parameters:
+//   key:     key to be stored in the KV store
+//   value:   value to be stored for the key
+//   marshal: function to be used to convert types.State to
+//            a byte slice
+//
+// Return values:
+//   error: Error while marshaling values for key or
+//          when writing the key-value pair to consul,
+//          nil if successful
+//
+func (d *ConsulStateDriver) WriteState(key string, value types.State,
+	marshal func(interface{}) ([]byte, error)) error {
+	key = processKey(key)
+	encodedState, err := marshal(value)
+	if err != nil {
+		return err
+	}
+
+	return d.Write(key, encodedState)
+}
---
+++ state/etcdstatedriver.go
@@ -0,0 +1,475 @@
+package state
+
+import (
+	"errors"
+	"reflect"
+	"strings"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/coreos/etcd/client"
+	"golang.org/x/net/context"
+
+	ccnerrors "github.com/contiv/ccn_proxy/common/errors"
+	"github.com/contiv/ccn_proxy/common/types"
+)
+
+const (
+
+	// timeout used for etcd client calls
+	ctxTimeout = 20 * time.Second
+
+	// max times to retry in case of failure
+	maxEtcdRetries = 10
+)
+
+// EtcdStateDriver implements the StateDriver interface for an etcd-based
+// distributed key-value store that is used to store any state information
+// needed by CCN proxy
+type EtcdStateDriver struct {
+
+	// Client to access etcd
+	Client client.Client
+
+	// KeysAPI is used to interact with etcd's key-value
+	// API over HTTP
+	KeysAPI client.KeysAPI
+}
+
+//
+// Init initializes the state driver with needed config
+//
+// Parameters:
+//   config: configuration parameters to create etcd client
+//
+// Return values:
+//   error:  error when creating an etcd client
+//
+func (d *EtcdStateDriver) Init(config *types.KVStoreConfig) error {
+	var err error
+
+	if config == nil || !strings.Contains(config.StoreURL, "etcd://") {
+		return errors.New("Invalid etcd config")
+	}
+
+	// configure etcd endpoints
+	etcdURL := strings.Replace(config.StoreURL, "etcd://", "http://", 1)
+	etcdConfig := client.Config{
+		Endpoints: []string{etcdURL},
+	}
+
+	// create etcd client
+	d.Client, err = client.New(etcdConfig)
+	if err != nil {
+		log.Fatalf("failed to create etcd client, err: %v", err)
+	}
+
+	// create keys api
+	d.KeysAPI = client.NewKeysAPI(d.Client)
+
+	return nil
+}
+
+// Deinit is currently a no-op
+func (d *EtcdStateDriver) Deinit() {}
+
+//
+// Write state (consisting of a key-value pair) to the etcd
+// KV store
+//
+// Parameters:
+//   key:    key to be stored
+//   value:  value to be stored
+//
+// Return values:
+//   error: Error when writing to the KeysAPI of etcd client
+//          nil if successful
+//
+func (d *EtcdStateDriver) Write(key string, value []byte) error {
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+
+	_, err := d.KeysAPI.Set(ctx, key, string(value[:]), nil)
+	if err != nil {
+		// Retry few times if cluster is unavailable
+		if err.Error() == client.ErrClusterUnavailable.Error() {
+			for i := 0; i < maxEtcdRetries; i++ {
+				_, err = d.KeysAPI.Set(ctx, key, string(value[:]), nil)
+				if err == nil {
+					break
+				}
+
+				// Retry after a delay
+				time.Sleep(time.Second)
+			}
+		}
+	}
+
+	return err
+}
+
+//
+// Read returns state for a key
+//
+// Parameters:
+//   key:    key for which value is to be retrieved
+//
+// Return values:
+//   []byte: value associated with the given key
+//   error: Error when writing to the KeysAPI of etcd client
+//          nil if successful
+//
+func (d *EtcdStateDriver) Read(key string) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+
+	resp, err := d.KeysAPI.Get(ctx, key, &client.GetOptions{Quorum: true})
+	if err != nil {
+		// Retry few times if cluster is unavailable
+		if err.Error() == client.ErrClusterUnavailable.Error() {
+			for i := 0; i < maxEtcdRetries; i++ {
+				resp, err = d.KeysAPI.Get(ctx, key, &client.GetOptions{Quorum: true})
+				if err == nil {
+					break
+				}
+
+				// Retry after a delay
+				time.Sleep(time.Second)
+			}
+		} else {
+			return []byte{}, err
+		}
+	}
+
+	return []byte(resp.Node.Value), err
+}
+
+//
+// ReadAll returns all values for a key
+//
+// Parameters:
+//   key:    key for which all values are to be retrieved
+//
+// Return values:
+//   [][]byte: slice of values associated with the given key
+//   error:    Error when writing to the KeysAPI of etcd client
+//             nil if successful
+//
+func (d *EtcdStateDriver) ReadAll(baseKey string) ([][]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+
+	resp, err := d.KeysAPI.Get(ctx, baseKey, &client.GetOptions{Recursive: true, Quorum: true})
+	if err != nil {
+		return nil, err
+	}
+
+	values := [][]byte{}
+	for _, node := range resp.Node.Nodes {
+		values = append(values, []byte(node.Value))
+	}
+
+	return values, nil
+}
+
+//
+// channelEtcdEvents
+//
+// Parameters:
+//   watcher:        Any struct that implements the Watcher interface provided
+//                   by the etcd client
+//   chValueChanges: Channel of type [2][]byte used to communicate the value changes
+//                   for a key in the KV store
+//
+func (d *EtcdStateDriver) channelEtcdEvents(watcher client.Watcher, chValueChanges chan [2][]byte) {
+	for {
+		// block on change notifications
+		etcdRsp, err := watcher.Next(context.Background())
+		if err != nil {
+			log.Errorf("Error %v during watch", err)
+			time.Sleep(time.Second)
+			continue
+		}
+
+		// The logic below assumes that the node returned is always a node
+		// of interest. Eg: If we set a watch on /a/b/c, then we are mostly
+		// interested in changes in that directory i.e. changes to /a/b/c/d1..d2
+		byteValues := [2][]byte{nil, nil}
+		eventStr := "create"
+
+		// store current node value
+		if etcdRsp.Node.Value != "" {
+			byteValues[0] = []byte(etcdRsp.Node.Value)
+		}
+
+		// store previous node value
+		if etcdRsp.PrevNode != nil && etcdRsp.PrevNode.Value != "" {
+			byteValues[1] = []byte(etcdRsp.PrevNode.Value)
+			if etcdRsp.Node.Value != "" {
+				eventStr = "modify"
+			} else {
+				eventStr = "delete"
+			}
+		}
+
+		log.Debugf("Observed event:%q for key: %s", eventStr, etcdRsp.Node.Key)
+
+		// send changes in values for the key to a channel
+		chValueChanges <- byteValues
+	}
+}
+
+//
+// WatchAll watches value changes for a key in etcd
+//
+// Parameters:
+//   baseKey:        key for which changes are to be watched
+//   chValueChanges: channel for communicating the changes in
+//                   the values for a key from this method
+//
+// Return values:
+//   error: Any error when watching for a state transition
+//          for a key
+//          nil if successful
+//
+func (d *EtcdStateDriver) WatchAll(baseKey string, chValueChanges chan [2][]byte) error {
+
+	watcher := d.KeysAPI.Watcher(baseKey, &client.WatcherOptions{Recursive: true})
+	if watcher == nil {
+		log.Errorf("etcd watch failed")
+		return errors.New("etcd watch failed")
+	}
+
+	go d.channelEtcdEvents(watcher, chValueChanges)
+
+	return nil
+}
+
+//
+// ClearState removes a key from etcd
+//
+// Parameters:
+//   key: key to be stored
+//
+// Return value:
+//   error: Error returned by etcd client when deleting a key
+//
+func (d *EtcdStateDriver) ClearState(key string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+
+	_, err := d.KeysAPI.Delete(ctx, key, nil)
+	return err
+}
+
+//
+// ReadState reads a key's value into a types.State struct using
+// the provided unmarshaling function.
+//
+// Parameters:
+//   key:       key whose value is to be retrieved
+//   value:     value of the key as types.State
+//   unmarshal: function to be used for unmarshaling the (byte
+//              slice) value into types.State struct
+//
+// Return value:
+//   error: Error returned by etcd client when reading key's value
+//          or error in unmarshaling key's value
+//
+func (d *EtcdStateDriver) ReadState(key string, value types.State,
+	unmarshal func([]byte, interface{}) error) error {
+
+	encodedState, err := d.Read(key)
+	if err != nil {
+		return err
+	}
+
+	return unmarshal(encodedState, value)
+}
+
+//
+// readAllStateCommon reads and unmarshals (given a function) all state into a
+// slice of type types.State
+//
+// Parameters:
+//   d:          StateDriver abstracting etcd or consul KV store
+//   baseKey:    key whose state is to be read from KV store
+//   sType:      State
+//   unmarshal:  Unmarshal function to convert key's value from a byte slice
+//               to a struct of type types.State
+//
+// Return value:
+//   []types.State: slice of states
+//   error:         Error returned when reading the key and unmarshaling it
+//                  nil if successful
+//
+func readAllStateCommon(d types.StateDriver, baseKey string, sType types.State,
+	unmarshal func([]byte, interface{}) error) ([]types.State, error) {
+
+	stateType := reflect.TypeOf(sType)
+	sliceType := reflect.SliceOf(stateType)
+	values := reflect.MakeSlice(sliceType, 0, 1)
+
+	byteValues, err := d.ReadAll(baseKey)
+	if err != nil {
+		return nil, err
+	}
+	for _, byteValue := range byteValues {
+		value := reflect.New(stateType)
+		err = unmarshal(byteValue, value.Interface())
+		if err != nil {
+			return nil, err
+		}
+		values = reflect.Append(values, value.Elem())
+	}
+
+	stateValues := []types.State{}
+	for i := 0; i < values.Len(); i++ {
+		// sanity checks
+		if !values.Index(i).Elem().FieldByName("CommonState").IsValid() {
+			return nil, ccnerrors.ErrCommonStateFieldsMissing
+		}
+
+		//the following works as every types.State is expected to embed core.CommonState struct
+		values.Index(i).Elem().FieldByName("CommonState").FieldByName("StateDriver").Set(reflect.ValueOf(d))
+		stateValue := values.Index(i).Interface().(types.State)
+		stateValues = append(stateValues, stateValue)
+	}
+	return stateValues, nil
+}
+
+//
+// ReadAllState returns all state for a key
+//
+// Parameters:
+//   baseKey:    key whose values are to be read
+//   sType:      types.State struct into which values are to be
+//               unmarshaled
+//   unmarshal:  function that is used to convert key's values to
+//               values of type types.State
+//
+// Return values:
+//   []types.State: slice of states for the given key
+//   error:         Any error returned by readAllStateCommon
+//                  nil if successful
+//
+func (d *EtcdStateDriver) ReadAllState(baseKey string, sType types.State,
+	unmarshal func([]byte, interface{}) error) ([]types.State, error) {
+	return readAllStateCommon(d, baseKey, sType, unmarshal)
+}
+
+//
+// channelStateEvents watches for updates (create, modify, delete) to a state of
+// specified type and unmarshals (given a function) all changes and puts them on
+// a channel of types.WatchState objects.
+//
+// Parameters:
+//    d:              StateDriver that abstracts access to etcd or consul
+//    sType:          types.State
+//    unmarshal:      function used to unmarshall byte slice values into
+//                    type types.State
+//    chValueChanges: channel of [2][]byte via which this method
+//                    returns any value changes that were observed in the KV store
+//    chStateChanges: channel of type types.WatchState via which this method
+//                    returns any state changes that were observed in the KV store
+//    chErr:          channel of type error via which this method returns
+//                    any errors encountered
+//
+func channelStateEvents(d types.StateDriver, sType types.State,
+	unmarshal func([]byte, interface{}) error,
+	chValueChanges chan [2][]byte,
+	chStateChanges chan types.WatchState,
+	chErr chan error) {
+
+	for {
+		// block on change notifications
+		byteRsp := <-chValueChanges
+
+		stateChange := types.WatchState{Curr: nil, Prev: nil}
+		for i := 0; i < 2; i++ {
+			if byteRsp[i] == nil {
+				continue
+			}
+			stateType := reflect.TypeOf(sType)
+			value := reflect.New(stateType)
+			err := unmarshal(byteRsp[i], value.Interface())
+			if err != nil {
+				chErr <- err
+				return
+			}
+			if !value.Elem().Elem().FieldByName("CommonState").IsValid() {
+				chErr <- ccnerrors.ErrCommonStateFieldsMissing
+				return
+			}
+
+			//the following works as every types.State is expected to embed core.CommonState struct
+			value.Elem().Elem().FieldByName("CommonState").FieldByName("StateDriver").Set(reflect.ValueOf(d))
+			switch i {
+			case 0:
+				stateChange.Curr = value.Elem().Interface().(types.State)
+			case 1:
+				stateChange.Prev = value.Elem().Interface().(types.State)
+			}
+		}
+
+		// send state changes for the key to a channel
+		chStateChanges <- stateChange
+	}
+}
+
+//
+// WatchAllState watches all state from the baseKey
+//
+// Parameters:
+//    baseKey:        key to be watched
+//    sType:          types.State struct to convert values to
+//    unmarshal:      function used to convert values to types.State
+//    chStateChanges: channel of types.WatchState
+//
+// Return values:
+//    error: Any error when watching all state
+//
+func (d *EtcdStateDriver) WatchAllState(baseKey string, sType types.State,
+	unmarshal func([]byte, interface{}) error, chStateChanges chan types.WatchState) error {
+
+	// channel that will be used to communicate value changes
+	// from the WatchAll function
+	chValueChanges := make(chan [2][]byte, 1)
+
+	// channel that will be used to communicate errors
+	// from the channelStateEvents method
+	chErr := make(chan error, 1)
+
+	go channelStateEvents(d, sType, unmarshal, chValueChanges, chStateChanges, chErr)
+
+	err := d.WatchAll(baseKey, chValueChanges)
+	if err != nil {
+		return err
+	}
+
+	err = <-chErr
+	return err
+}
+
+//
+// WriteState writes a value of types.State for a key in the KV store
+//
+// Parameters:
+//   key:   key to be stored in the KV store
+//   value: value as types.State
+//   marshal: function to be used to convert types.State to a form
+//            that can be stored in the KV store
+//
+// Return values:
+//   error: Error while marshaling or writing a key-value pair
+//          to the KV store
+//
+func (d *EtcdStateDriver) WriteState(key string, value types.State,
+	marshal func(interface{}) ([]byte, error)) error {
+	encodedState, err := marshal(value)
+	if err != nil {
+		return err
+	}
+
+	return d.Write(key, encodedState)
+}
