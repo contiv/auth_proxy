4b9e716034d7580b43d0a093ecc2d16f17db84e5
Bill Robinson <dseevr@users.noreply.github.com>
2017-01-10 16:04:08 -0800

    Fixed a bug where the BUILD_VERSION was not being properly passed to the containerized build process
    Signed-off-by: Bill Robinson <dseevr@users.noreply.github.com>

--- scripts/build-in-container.sh
+++ scripts/build-in-container.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euxo pipefail
 
 # disabling CGO makes `go build` generate a static binary which is what we want
 # for our "scratch"-based final image.
 export CGO_ENABLED=0
-
-VERSION=${BUILD_VERSION-devbuild}
 
 # output the binary under the build/output directory in the code dir so that it's
 # available when we issue the `docker build` and COPY it into the final image.
@@ -16,3 +14,5 @@ go build \
    github.com/contiv/ccn_proxy
 
 strip ./build/output/ccn_proxy
+
+docker build -t $IMAGE_NAME:$VERSION -f ./build/Dockerfile.release .
