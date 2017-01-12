50f5f36a90caaf05a32382fda829562976afd5b6
Neelima Mukiri <neelimam@gmail.com>
2017-01-11 20:05:41 -0800

    Run install container from any host
    Also split the netplugin nodes into worker & master categories.
    netmaster, swarm manager and etcd master nodes are run on
    netplugin-master nodes.

--- install/README.md
+++ install/README.md
@@ -3,30 +3,32 @@
 ## Docker Swarm installation
 
 ### Pre-requisites
-* Supported operating systems are CentOS 7.x or RHEL 7.x
+* Supported operating systems are CentOS 7.x or RHEL 7.x.
+* Docker needs to be installed on the host where the installer is being run.
 * Install a docker swarm cluster (or)
-  Install docker on the netmaster node and use the UCN installer to install the scheduler stack.
-  This requires the docker-tcp.socket to be enabled and active.
+  Use the UCN installer to install the scheduler stack after installing docker
+  on a node external to the cluster where the scheduler stack is being installed.
 
 ### UCN Installation
 
 * Download the install bundle `<TODO add a location here>`
-* Extract the install bundle
-* Run `./install/ansible/install_swarm.sh "-n $ucn_master -f install/ansible/cfg.yml"` to install UCN without the scheduler stack
-* Run `./install/ansible/install_swarm.sh "-n $ucn_master -f install/ansible/cfg.yml -i"` to install UCN with the scheduler stack
-* To see additional install options run `./install/ansible/install_swarm.sh`
+* Extract the install bundle and cd to the extracted folder.
+* To load the installer container image run `docker load -i ucn-install-image.tar`
+* Run `./install/ansible/install_swarm.sh -f install/ansible/cfg.yml -n 192.168.2.12 -e cluster/export/insecure_private_key` to install UCN without the scheduler stack.
+* Run `./install/ansible/install_swarm.sh -f install/ansible/cfg.yml -n 192.168.2.12 -e cluster/export/insecure_private_key -i` to install UCN with the scheduler stack.
+* To see additional install options run `./install/ansible/install_swarm.sh`.
 
 ## Kubernetes 1.4 installation
 
 ### Pre-requisites
 
-* Supported operating systems are CentOS 7.x or RHEL 7.x
-* Install kubernetes 1.4 or higher using http://kubernetes.io/docs/getting-started-guides/kubeadm/
+* Supported operating systems are CentOS 7.x or RHEL 7.x.
+* Install kubernetes 1.4 or higher using http://kubernetes.io/docs/getting-started-guides/kubeadm/.
 
 ### UCN Installation
 
-* Download the install bundle  `<TODO add a location here>`
-* Extract the install bundle
-* Run `sudo ./install/k8s/install.sh -n $ucn_master`
-  where $ucn_master is the IP to be used for the UCN proxy
-* To see additional install options run `./install/ansible/install.sh`
+* Download the install bundle  `<TODO add a location here>`.
+* Extract the install bundle.
+* Run `sudo ./install/k8s/install.sh -n $ucn_master`.
+  where $ucn_master is the IP to be used for the UCN proxy.
+* To see additional install options run `./install/ansible/install.sh`.
--- install/ansible/install_swarm.sh
+++ install/ansible/install_swarm.sh
@@ -1,19 +1,120 @@
-docker_version="1.11.1"
+# This is the installation script for Cisco Unified Container Networking platform.
 
-echo "Checking for pre-requisites"
+. ./install/ansible/install_defaults.sh
 
-echo "Check that docker $docker_version is installed"
-docker_status=`docker --version | grep "Docker version $docker_version" -o`
-if [ "$docker_status" != "Docker version $docker_version" ]; then
-  echo "Install docker $docker_version"
+# Ansible options. By default, this specifies a private key to be used and the vagrant user
+def_ans_opts="--private-key $ans_key -u vagrant"
+
+# Check for docker version and status
+check_for_prereqs() {
+  echo "Checking for pre-requisites"
+  success=True
+  docker_version=${DOCKER_VERSION:-"1.11.1"}
+  echo "Check that docker $docker_version is installed"
+  docker_status=`docker --version | grep "Docker version $docker_version" -o`
+  if [ "$docker_status" != "Docker version $docker_version" ]; then
+    echo "Expected $docker_version, but found $docker_status."
+    success=False
+  fi
+
+  docker_status=`systemctl status docker-tcp.socket | grep 'Active.*active' -o`
+  if [ "$docker_status" != "Active: active" ]; then
+    echo "docker-tcp.socket is not active."
+    success=False
+  fi
+  if [ $success = False ]; then 
+    echo "This can cause problems installing docker if the script is being run from a node in the cluster."
+    echo "If you are running the script from a node not being used for the installation, you may continue with the installation."
+    echo "Otherwise install $docker_version with docker-tcp.socket active and retry the installation."
+    echo "Press Ctrl+C to cancel the installation"
+    sleep 5
+  fi
+}
+
+function usage {
+  echo "Usage:"
+  echo "./install_swarm.sh -f <host configuration file> -n <netmaster IP> -a <ansible options> -e <ansible key> -i <install scheduler stack> -z <installer config file>"
+
+  echo ""
   exit 1
+}
+
+mkdir -p $src_conf_path
+# Check for docker only when requested to install the scheduler stack
+# Else there are no pre-requisites on the host
+install_scheduler=""
+while getopts ":f:z:c:k:n:a:e:i" opt; do
+  case $opt in
+    f)
+      cp $OPTARG $host_contiv_config
+      ;;
+    z)
+      cp $OPTARG $host_installer_config
+      ;;
+    c)
+      cp $OPTARG $host_tls_cert
+      ;;
+    k)
+      cp $OPTARG $host_tls_key
+      ;;
+    n)
+      netmaster=$OPTARG
+      ;;
+    a)
+      ans_opts=$OPTARG
+      ;;
+    e)
+      ans_key=$OPTARG
+      ;;
+    i) 
+      check_for_prereqs
+      install_scheduler="-i"
+      ;;
+    :)
+      echo "An argument required for $OPTARG was not passed"
+      usage
+      ;;
+    ?)
+      usage
+      ;;
+  esac
+done
+
+if [[ ! -f $host_contiv_config ]]; then
+	echo "Host configuration file missing"
+  usage
 fi
 
-docker_status=`systemctl status docker-tcp.socket | grep 'Active.*active' -o`
-if [ "$docker_status" != "Active: active" ]; then
-  echo "Install docker with tcp socket and rerun the UCN installer"
-  exit 1
+if [[ ! -f $host_installer_config ]]; then
+	touch $host_installer_config
+  echo "Created host_installer_config"
 fi
+
+if [[ "$netmaster" = ""  ]]; then
+	echo "Netmaster IP/name is missing"
+  usage
+fi
+
+if [[ -f $ans_key ]]; then
+  cp $ans_key $host_ans_key
+fi
+if [[ "$ans_opts" = "" ]]; then
+	echo "Attempting to install with default Ansible SSH options $def_ans_opts"
+  ans_opts=$def_ans_opts
+fi
+
+if [[ ! -f $host_tls_cert || ! -f $host_tls_key ]]; then
+  echo "Generating local certs for UCN Proxy"
+  openssl genrsa -out $host_tls_key 2048 >/dev/null 2>&1
+  openssl req -new -x509 -sha256 -days 3650 \
+      -key $host_tls_key \
+      -out $host_tls_cert \
+      -subj "/C=US/ST=CA/L=San Jose/O=CPSG/OU=IT Department/CN=ccn-local.cisco.com"
+fi 
+
+# Copy the proxy container image to the host share path
+cp ucn-proxy-image.tar $src_conf_path
 
 echo "Starting the ansible container"
-sudo docker run -v /shared/:/contiv/config -v /var/contiv:/var/contiv contiv/install:devbuild sh -c "./install/ansible/install.sh $@"
+docker run -v $src_conf_path:$container_conf_path contiv/install:devbuild sh -c "./install/ansible/install.sh -n $netmaster -a \"$ans_opts\" $install_scheduler"
+rm -rf $src_conf_path
